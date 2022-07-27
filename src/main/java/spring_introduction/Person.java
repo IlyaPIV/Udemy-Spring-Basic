7ec8d056e8d9540c804e383f17f25e5aed2f4965
p_Ilya <petermoloch@mail.ru>
2022-07-27 21:15:25 +0300

    Dependency Injection - setters

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -7,6 +7,15 @@ public class Person {
         this.pet = pet;
     }
 
+    public Person() {
+        System.out.println(">> Person Bean is created");
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println(">> class Person: set pet");
+        this.pet = pet;
+    }
+
     public void callYourPet(){
         System.out.println("Hey, pet, where are you?");
         pet.say();
