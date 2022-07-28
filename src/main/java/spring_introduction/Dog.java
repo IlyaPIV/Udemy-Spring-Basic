8ae26ac54a09b454e55a1a529b4ded84fdc49b68
p_Ilya <petermoloch@mail.ru>
2022-07-28 21:59:52 +0300

    Beans life cycle - init and destroy

--- src/main/java/spring_introduction/Dog.java
+++ src/main/java/spring_introduction/Dog.java
@@ -4,4 +4,12 @@ public class Dog implements Pet{
     public void say() {
         System.out.println("Bow-wow!");
     }
+
+    public void init(){
+        System.out.println("!!! class Dog - init method");
+    }
+
+    public void destroy(){
+        System.out.println("!!! class Dog - destroy method");
+    }
 }
