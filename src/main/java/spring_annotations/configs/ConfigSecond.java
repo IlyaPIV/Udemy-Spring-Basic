7383908ed7e0d53592adc7cfbec0114ec5a3b795
p_Ilya <petermoloch@mail.ru>
2022-07-29 08:56:20 +0300

    Config with java code. second method

---
+++ src/main/java/spring_annotations/configs/ConfigSecond.java
@@ -0,0 +1,26 @@
+package spring_annotations.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import spring_introduction.Cat;
+import spring_introduction.Person;
+import spring_introduction.Pet;
+
+@Configuration
+public class ConfigSecond {
+
+    @Bean
+    @Scope("prototype")
+    public Pet catBean(){
+        return new Cat();
+    }
+
+    @Bean
+    @Scope("singleton")
+    public Person personBean(){
+        return new Person(catBean());
+    }
+
+
+}
