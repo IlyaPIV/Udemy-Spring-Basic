bbcd59ce497ed4112ec2b74c32b4ba73062af74c
p_Ilya <petermoloch@mail.ru>
2022-07-29 08:59:13 +0300

    @PropertySource

--- src/main/java/spring_annotations/configs/ConfigSecond.java
+++ src/main/java/spring_annotations/configs/ConfigSecond.java
@@ -2,12 +2,14 @@ package spring_annotations.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 import spring_introduction.Cat;
 import spring_introduction.Person;
 import spring_introduction.Pet;
 
 @Configuration
+@PropertySource("classpath:myAppValues.properties")
 public class ConfigSecond {
 
     @Bean
