0c5b0f7e6b83e3c0ef266bf972bd86a5c7f42b54
p_Ilya <petermoloch@mail.ru>
2022-07-30 13:04:00 +0300

    aspects @Order

--- src/main/java/spring_aop/aspects/LoggingAndSecurityAspect.java
+++ src/main/java/spring_aop/aspects/LoggingAndSecurityAspect.java
@@ -5,8 +5,8 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
-@Component
-@Aspect
+//@Component
+//@Aspect
 public class LoggingAndSecurityAspect {
 
 //    @Pointcut("execution(* get*())")
@@ -24,53 +24,58 @@ public class LoggingAndSecurityAspect {
 //        System.out.println("beforeGetSecurityAdvice: проверка прав на возможность получения чего-либо");
 //    }
 
-    @Pointcut("execution(* spring_aop.UniversityLibrary.get*())")
-    private void allGetMethodsUniLibrary(){}
-
-    @Before("allGetMethodsUniLibrary()")
-    public void beforeGetLoggingAdvice(){
-        System.out.println("!beforeGetLoggingAdvice: writing Log #1");
-    }
-
-    @Pointcut("execution(* spring_aop.UniversityLibrary.return*())")
-    private void allReturnMethodsUniLibrary(){}
-
-    @Before("allReturnMethodsUniLibrary()")
-    public void beforeReturnLoggingAdvice(){
-        System.out.println("!beforeReturnLoggingAdvice: writing Log #2");
-    }
-
-    @Pointcut("execution(* spring_aop.UniversityLibrary.add*())")
-    private void allAddMethodsUniLibrary(){}
-
-    @Before("allAddMethodsUniLibrary()")
-    public void beforeAddLoggingAdvice(){
-        System.out.println("!beforeAddLoggingAdvice: writing Log #3");
-    }
-
-    @Pointcut("allGetMethodsUniLibrary() || allReturnMethodsUniLibrary()")
-    private void allGetAndReturnMethodsUniLibrary(){}
-
-    @Before("allGetAndReturnMethodsUniLibrary()")
-    public void beforeGetAndReturnLoggingAdvice(){
-        System.out.println("!beforeGetAndReturnLoggingAdvice: writing Log #4");
-    }
-
-    /*
-    *
+    /**
+     * КОМБИНАЦИИ Pointcut
      */
 
-    @Pointcut("execution(* spring_aop.UniversityLibrary.*(..))")
-    private void allUniLibraryMethods(){}
 
-    @Pointcut("execution(* spring_aop.UniversityLibrary.returnMagazine(..))")
-    private void returnMagazineUniLibraryMethod(){}
-
-    @Pointcut("allUniLibraryMethods() && !returnMagazineUniLibraryMethod()")
-    private void allMethodsExceptReturnMagazineUniLibrary(){}
-
-    @Before("allMethodsExceptReturnMagazineUniLibrary()")
-    public void allMethodsExceptReturnMagazineAdvice(){
-        System.out.println("!!!allMethodsExceptReturnMagazineAdvice: writing log #5");
-    }
+//    @Pointcut("execution(* spring_aop.UniversityLibrary.get*())")
+//    private void allGetMethodsUniLibrary(){}
+//
+//    @Before("allGetMethodsUniLibrary()")
+//    public void beforeGetLoggingAdvice(){
+//        System.out.println("!beforeGetLoggingAdvice: writing Log #1");
+//    }
+//
+//    @Pointcut("execution(* spring_aop.UniversityLibrary.return*())")
+//    private void allReturnMethodsUniLibrary(){}
+//
+//    @Before("allReturnMethodsUniLibrary()")
+//    public void beforeReturnLoggingAdvice(){
+//        System.out.println("!beforeReturnLoggingAdvice: writing Log #2");
+//    }
+//
+//    @Pointcut("execution(* spring_aop.UniversityLibrary.add*())")
+//    private void allAddMethodsUniLibrary(){}
+//
+//    @Before("allAddMethodsUniLibrary()")
+//    public void beforeAddLoggingAdvice(){
+//        System.out.println("!beforeAddLoggingAdvice: writing Log #3");
+//    }
+//
+//    @Pointcut("allGetMethodsUniLibrary() || allReturnMethodsUniLibrary()")
+//    private void allGetAndReturnMethodsUniLibrary(){}
+//
+//    @Before("allGetAndReturnMethodsUniLibrary()")
+//    public void beforeGetAndReturnLoggingAdvice(){
+//        System.out.println("!beforeGetAndReturnLoggingAdvice: writing Log #4");
+//    }
+//
+//    /*
+//    *
+//     */
+//
+//    @Pointcut("execution(* spring_aop.UniversityLibrary.*(..))")
+//    private void allUniLibraryMethods(){}
+//
+//    @Pointcut("execution(* spring_aop.UniversityLibrary.returnMagazine(..))")
+//    private void returnMagazineUniLibraryMethod(){}
+//
+//    @Pointcut("allUniLibraryMethods() && !returnMagazineUniLibraryMethod()")
+//    private void allMethodsExceptReturnMagazineUniLibrary(){}
+//
+//    @Before("allMethodsExceptReturnMagazineUniLibrary()")
+//    public void allMethodsExceptReturnMagazineAdvice(){
+//        System.out.println("!!!allMethodsExceptReturnMagazineAdvice: writing log #5");
+//    }
 }
--- src/main/java/spring_aop/aspects/LoggingAspect.java
+++ src/main/java/spring_aop/aspects/LoggingAspect.java
@@ -1,37 +1,48 @@
 package spring_aop.aspects;
 
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.aspectj.lang.annotation.Aspect;
 
-//@Component
-//@Aspect
+@Component
+@Aspect
+@Order(1)
 public class LoggingAspect {
 
-    @Before("execution(public void getBook())")
-    public void beforeGetBookAdvice(){
-        System.out.println("beforeGetBookAdvice: попытка получить книгу");
-    }
+//    @Before("execution(public void getBook())")
+//    public void beforeGetBookAdvice(){
+//        System.out.println("beforeGetBookAdvice: попытка получить книгу");
+//    }
+//
+//    @Before("execution(void getBook(spring_aop.Book, ..))")
+//    public void beforeGetBookLinkAdvice(){
+//        System.out.println("beforeGetBookLinkAdvice: попытка получить конкретную книгу");
+//    }
+//
+//    @Before("execution(public void spring_aop.SchoolLibrary.getBook())")
+//    public void beforeGetBookExtraAdvice(){
+//        System.out.println("beforeGetBookExtraAdvice: extra метод для SchoolLibrary");
+//    }
+//
+//    //@Before("execution(void get*(*))")  //с любым 1 параметром
+//    @Before("execution(* get*(..))") //с любым кол-вом параметров
+//    public void beforeGetSomethingAdvice(){
+//        System.out.println("beforeGetSomethingAdvice: попытка что-то взять");
+//    }
+//
+//    @Before("execution(public void returnBook())")
+//    public void beforeReturnBookAdvice(){
+//        System.out.println("beforeReturnBookAdvice: попытка вернуть книгу");
+//    }
 
-    @Before("execution(void getBook(spring_aop.Book, ..))")
-    public void beforeGetBookLinkAdvice(){
-        System.out.println("beforeGetBookLinkAdvice: попытка получить конкретную книгу");
-    }
+    /**
+     *
+     */
 
-    @Before("execution(public void spring_aop.SchoolLibrary.getBook())")
-    public void beforeGetBookExtraAdvice(){
-        System.out.println("beforeGetBookExtraAdvice: extra метод для SchoolLibrary");
-    }
 
-    //@Before("execution(void get*(*))")  //с любым 1 параметром
-    @Before("execution(* get*(..))") //с любым кол-вом параметров
-    public void beforeGetSomethingAdvice(){
-        System.out.println("beforeGetSomethingAdvice: попытка что-то взять");
-    }
-
-    @Before("execution(public void returnBook())")
-    public void beforeReturnBookAdvice(){
-        System.out.println("beforeReturnBookAdvice: попытка вернуть книгу");
-    }
-
+     @Before("spring_aop.aspects.MyPointcuts.allGetMethods()")
+     public void beforeGetLoggingAdvice(){
+        System.out.println("beforeGetLoggingAdvice: попытка что-то взять");
+     }
 }
